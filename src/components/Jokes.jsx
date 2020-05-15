8e7229518193be29321a1ef0d050187c9a14649f
github username <github email address>
2020-05-15 04:20:19 -0700

    update Jokes.jsx

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -2,33 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-
-  ${"" /* &:hover {
-    box-shadow: 0 10px 15px rgba(0, 0, 0, 0.25), 0 10px 15px rgba(0, 0, 0, 0.22);
-  } */}
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
 `;
 
 const Jokes = ({ jokes }) => {
   return (
-    <div className="col-sm-12 mb-5">
-      <div className="card-header">
-        <h4 style={{ textAlign: "center" }}>{"Chuck Norris Jokes"}</h4>
+    <div className="container">
+      <div className="col-sm-12 mt-5 mb-5 pr-5 pl-5">
+        <Card className="card text-center mx-auto">
+          <div className="card-header">
+            <h4 style={{ textAlign: "center" }}>{"Chuck Norris Jokes"}</h4>
+          </div>
+          <div className="card-body">
+            <img
+              src={jokes.icon_url}
+              alt="Chuck Norris"
+              className="img-thumbnail"
+            />
+            <p className="card-text">{jokes.value}</p>
+            <a href="." className="btn btn-primary">
+              New joke?
+            </a>
+          </div>
+        </Card>
       </div>
-      <Card className="card text-center">
-        <div className="card-body">
-          <img
-            src={jokes.icon_url}
-            alt="Chuck Norris"
-            className="img-thumbnail"
-          />
-          <p className="card-text">{jokes.value}</p>
-          <a href="." className="btn btn-primary">
-            New joke?
-          </a>
-        </div>
-      </Card>
     </div>
   );
 };
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Jokes from "../components/Jokes";
-// import "../styles/App.css";
 
 class App extends Component {
   state = {
